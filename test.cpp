9c846759c4295d2c6611508797428c6588c2f5ca
IsaacSJones <79865738+IsaacSJones@users.noreply.github.com>
2023-04-16 21:56:06 -0400

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,7 +1,10 @@
 #include "pch.h"
 #include <iostream>
+#include <fstream>
 #include <sstream>
 #include <map>
+#include <random>
+#include <chrono>
 #include <cstdlib> // for rand() function
 #include <ctime>   // for time() function
 
@@ -9,6 +12,14 @@ using namespace std;
 
 // Translates any predefined morse code input into a language
 // Written by Kristin
+
+// Returns a random ascii image if a morse input is not recognized
+string badInput() {
+    string reaction[] = { "| o_o |" , "( o.o )", "|o o  |", "bro... c'mon." };
+    srand(time(nullptr));
+    int x = rand() % 4;
+    return reaction[x];;
+}
 
 string translator(string input) {
     // Morse code map
@@ -40,85 +51,101 @@ string translator(string input) {
         {"-.--", "y"},
         {"--..", "z"}
     };
-    
+
+    string morseInput;
     stringstream ss(morseInput);
     string code, result = "";
     while (ss >> code) {
         if (morseMap.find(code) != morseMap.end()) {
             result += morseMap[code];
-        } else {
-			// If code is incorrect, program shames you.
-			string reaction[] = { "| o_o |" , "( o.o )", "|o o  |", "bro... c'mon."};
-			srand(time(nullptr));
-			int x = rand() % 4;
-            result = reaction[x];
+        }
+        else {
+            // If code is incorrect, program shames you.
+            result = badInput();
             break;
         }
     }
     return result;
 }
 
-// Returns a random ascii image if a morse input is not recognized
-string badInput() {
-	return "";
-}
-
 bool findWordInFile(char filename[], string word) {
-    // function code here
-	return 0;
+    string list;
+    ifstream wordList(filename);
+
+    if (!wordList.is_open()) {
+        cout << "Unable to open file wordList." << endl;
+        return false;
+    }
+
+    string line;
+    while (getline(wordList, line)) {
+        if (line.find(word) == false) {
+            return true;
+        }
+    }
+
+    return false;
+
 }
 
 // Function that generates a random math problem
-string generateMathProblem() {
-  	return "";
+bool solveMathProblem(int num1, int num2, int input) {
+    int solution = num1 + num2;
+
+    if (input == solution) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 // Function that checks if a given string is a valid math problem
-bool isMathProblem(const string& str) {
-  	return 0;
-}
+//bool isMathProblem(const string& str) {
+//    return 0;
+//}
 
 TEST(MorseTest, TranslationTest1) {
 
-	string morseInput = "- .-- . ...";
-	string expected = "yes";
-	
-	EXPECT_EQ(translator(morseInput), expected);
+    string morseInput = "- .-- . ...";
+    string expected = "yes";
+
+    EXPECT_EQ(translator(morseInput), expected);
 }
 
 TEST(MorseTest, TranslationTest2) {
 
-	string morseInput = "-. ---";
-	string expected = "no";
+    string morseInput = "-. ---";
+    string expected = "no";
 
-	EXPECT_EQ(translator(morseInput), expected);
+    EXPECT_EQ(translator(morseInput), expected);
 }
 
 TEST(MorseTest, NoTranslationTest1) {
 
-	string morseInput = "--------"; // Something that we dont plan on recognizing
-	string reactionExample[] = { "| o_o |" , "( o.o )", "|o o  |", "bro... c'mon."}; // Placeholder things to compare to
-	string expected = "no";
+    string morseInput = "--------"; // Something that we dont plan on recognizing
+    string reactionExample[] = { "| o_o |" , "( o.o )", "|o o  |", "bro... c'mon." }; // Placeholder things to compare to
+    string expected = "no";
 
-	// No clue how to write this one well, if it works then it should throw a fail for every reaction that isnt matched but one will
-	// If nothing matches then the test overall fails
-	// IF multiple things match... uh oh.
-	for (int i = 0; i < 4; ++i) {
-		EXPECT_EQ(translator(morseInput), reactionExample[i]);
-	}
+    // No clue how to write this one well, if it works then it should throw a fail for every reaction that isnt matched but one will
+    // If nothing matches then the test overall fails
+    // IF multiple things match... uh oh.
+    for (int i = 0; i < 4; ++i) {
+        EXPECT_EQ(translator(morseInput), reactionExample[i]);
+    }
 
 }
 
 // Tests for Finding a word in file written by Kristin
 TEST(FileSearchTest, TestFoundWordInFile) {
-    char filename[] = "example.txt";
+    char filename[] = "wordList.txt";
     string desiredWord = "hello";
     bool found = findWordInFile(filename, desiredWord);
     ASSERT_TRUE(found);
 }
 
 TEST(FileSearchTest, TestNotFoundWordInFile) {
-    char filename[] = "example.txt";
+    char filename[] = "wordList.txt";
     string nonDesiredWord = "world";
     bool notFound = findWordInFile(filename, nonDesiredWord);
     ASSERT_FALSE(notFound);
@@ -136,28 +163,33 @@ TEST(FileSearchTest, TestNonExistingFile) {
 // Test fixture for the math problem feature
 class MathProblemTest : public ::testing::Test {
 protected:
-  void SetUp() override {
-    // Seed the random number generator
-    srand(time(nullptr));
-  }
+    void SetUp() override {
+        // Seed the random number generator
+        srand(time(nullptr));
+    }
 };
 
 // Test case that checks if a generated math problem is valid
-TEST_F(MathProblemTest, GeneratesValidMathProblem) {
-	string problem = generateMathProblem();
-  	EXPECT_TRUE(isMathProblem(problem));
+TEST_F(MathProblemTest, MathProblemPass) {
+    //string problem = generateMathProblem();
+    mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+    uniform_int_distribution<int> dist(1, 100);
+    int num1 = dist(rng);
+    int num2 = dist(rng);
+
+    int input = num1 + num2;
+
+    EXPECT_TRUE(solveMathProblem(num1, num2, input));
 }
 
 // Test case that checks if a user input is a valid math problem
-TEST_F(MathProblemTest, UserInputIsMathProblem) {
-	// Simulate user input
-	string userInput = "2+3?";
-	EXPECT_TRUE(isMathProblem(userInput));
-}
+TEST_F(MathProblemTest, MathProblemFail) {
+    mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+    uniform_int_distribution<int> dist(1, 100);
+    int num1 = dist(rng);
+    int num2 = dist(rng);
 
-// Test case that checks if a user input is not a valid math problem
-TEST_F(MathProblemTest, UserInputIsNotMathProblem) {
-	// Simulate user input
-	string userInput = "hello world";
-	EXPECT_FALSE(isMathProblem(userInput));
+    int input = (num1 + num2) + 1;
+
+    EXPECT_FALSE(solveMathProblem(num1, num2, input));
 }
